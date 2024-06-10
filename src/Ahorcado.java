6bc4d29823f460dbb4088a32dc81b5de27f5ab67
23SebastianDiaz <sdiaz6376@gmail.com>
2024-06-10 16:51:41 -0500

    Actulizacion README

--- src/Ahorcado.java
+++ src/Ahorcado.java
@@ -27,29 +27,30 @@ public class Ahorcado {
             // Devuelve solo la primera letra que escriba, en minuscula.
             char letra = Character.toLowerCase(scanner.next().charAt(0));
 
-            boolean letraAdivinada = false;
+            boolean letraAdivinada = false; 
 
             // Bucle para las palabras adivinadas
             for (int i = 0; i < palabraSecreta.length(); i++) {
                 if (palabraSecreta.charAt(i) == letra) { // Si alguna letra de la palabra es igual a la letra ingresada.
                     letrasCorrectas[i] = letra; // Asigne en el indice que adivino la letra.
-                    letraAdivinada = true; // Cambia estado de letraCorrecta para que no sume intentos
+                    letraAdivinada = true; // Cambia estado de letraCorrecta para que no sume intentos.
                 }
             }
-
             // Condicion para restar numeros de intentos si la letra no fue correcta (se
-            // mantiene en false)
+            // mantiene en false).
             if (!letraAdivinada) {
-                intentos++; // Suma los intentos
+                intentos++; // Suma los intentos.
                 System.out.println("Incorrecto! Te quedan " + (intentosMax - intentos) + " intentos.");
             }
 
+            // Condicion para cuando las todos lo char de letras correctar sean iguales a la palabra secreta.
             if (String.valueOf(letrasCorrectas).equals(palabraSecreta)) {
                 palabraCorrecta = true;
                 System.out.println("Felicidades! Has adivinado la palabra secreta: " + palabraSecreta);
             }
-        }
+        } //Cierra While
 
+        // Condicion para cuando se acaben los intentos segun la descicion del while.
         if (!palabraCorrecta) {
             System.out.println("Que pena te has quedado sin intentos! GAME OVER.");
         }
