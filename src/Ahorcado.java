dc77fdeb4a477bdf02d5ec2095d8208631f94c4c
23SebastianDiaz <sdiaz6376@gmail.com>
2024-06-10 16:34:15 -0500

    Juego Ahorcado en JAVA

---
+++ src/Ahorcado.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Ahorcado {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String palabraSecreta = "disciplina";
+        int intentosMax = 10;
+        int intentos = 0;
+        boolean palabraCorrecta = false;
+
+        char[] letrasCorrectas = new char[palabraSecreta.length()]; // Cuantas letras necesitan ser adiviandas.
+
+        // imprime la cantidad de guiones.
+        for (int i = 0; i < letrasCorrectas.length; i++) {
+            letrasCorrectas[i] = '_';
+        }
+
+        // Recorre todo el juego hasta que adivine la palabra o pierda por intentos.
+        while (!palabraCorrecta && intentos < intentosMax) {
+                                                    //Se usa cuando tenemos una palabras de chars 
+            System.out.println("Palabra a adivinar: " + String.valueOf(letrasCorrectas) + " (" + palabraSecreta.length()
+                    + " Letras)");
+            System.out.println("Introduce un letra, por favor.");
+
+            // Devuelve solo la primera letra que escriba, en minuscula.
+            char letra = Character.toLowerCase(scanner.next().charAt(0));
+
+            boolean letraAdivinada = false;
+
+            // Bucle para las palabras adivinadas
+            for (int i = 0; i < palabraSecreta.length(); i++) {
+                if (palabraSecreta.charAt(i) == letra) { // Si alguna letra de la palabra es igual a la letra ingresada.
+                    letrasCorrectas[i] = letra; // Asigne en el indice que adivino la letra.
+                    letraAdivinada = true; // Cambia estado de letraCorrecta para que no sume intentos
+                }
+            }
+
+            // Condicion para restar numeros de intentos si la letra no fue correcta (se
+            // mantiene en false)
+            if (!letraAdivinada) {
+                intentos++; // Suma los intentos
+                System.out.println("Incorrecto! Te quedan " + (intentosMax - intentos) + " intentos.");
+            }
+
+            if (String.valueOf(letrasCorrectas).equals(palabraSecreta)) {
+                palabraCorrecta = true;
+                System.out.println("Felicidades! Has adivinado la palabra secreta: " + palabraSecreta);
+            }
+        }
+
+        if (!palabraCorrecta) {
+            System.out.println("Que pena te has quedado sin intentos! GAME OVER.");
+        }
+
+        scanner.close();
+
+    }
+}
